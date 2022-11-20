d794d971cf969daa46cce6367b124fa264929898
nandansn <geeknanda13@gmail.com>
2022-11-20 12:29:16 +0530

    updated

---
+++ js/array-test.js
@@ -0,0 +1,23 @@
+let nums = [1,2,3]
+
+nums.shift()
+
+console.log(nums);
+
+nums.unshift(1)
+
+console.log(nums);
+
+
+[1,2,3].concat([4,5,6]).forEach(i => console.log(i))
+
+
+
+
+nums.splice(0,0,789)
+
+console.log(nums);
+
+
+console.log(nums.slice(2,5))
+
